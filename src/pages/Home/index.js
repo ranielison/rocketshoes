aaf38fc2eaeab5a36ce74c2160f9a86cab284e50
Ranielison <ranielisonsoares2@gmail.com>
2019-10-16 11:12:16 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
+import { connect } from "react-redux";
+import { ProductList } from "./styles";
+import api from "../../services/api";
+import { formatPrice } from "../../util/format";
+
+class Home extends React.Component {
+  state = {
+    products: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("products");
+
+    const data = response.data.map(product => ({
+      ...product,
+      price: formatPrice(product.price)
+    }));
+
+    this.setState({ products: data });
+  }
+
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: "ADD_TO_CART",
+      product
+    });
+  };
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
+}
+
+export default connect()(Home);
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/index";
+import Cart from "./pages/Cart/index";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/cart" component={Cart}></Route>
+    </Switch>
+  );
+}
