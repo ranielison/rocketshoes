242ad8127a5f977257c9840de0e97e7829385715
Ranielison <ranielisonsoares2@gmail.com>
2019-10-20 02:03:44 -0300

    fiz a configuraçao do reactotron e immer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import Routes from "./routes";
+
+import "./config/ReactotronConfig";
+
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header/index";
-
 import store from "./store/index";
 
 function App() {
